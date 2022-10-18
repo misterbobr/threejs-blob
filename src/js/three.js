430f710bc8c9ac13e7408b09d6e3b59dca9db9e5
misterbobr <monsieurraspberry@gmail.com>
2022-10-19 04:24:54 +0600

    Coal blob

--- src/js/three.js
+++ src/js/three.js
@@ -13,27 +13,29 @@ gsap.registerPlugin(CustomEase);
 let renderer,
 scene,
 sizes,
+aspectRatio,
 camera,
 raycaster,
 pointer,
 aLight,
 defaultColor,
-dLightTop,
+radius,
+vertexNormals,
 rows,
 cols,
 meshes,
 controls,
 intersectedObjects,
+scale,
 container = document.getElementById("canvas_container"),
 noise = new createNoise3D(),
 velocities,
 clock,
-blobScale = 1.5;
+blobScale = 0.3;
 
 function init() {
     scene = new THREE.Scene();
 
-    scene.background = 
     // RESIZE EVENT
     sizes = {
         width: window.innerWidth,
@@ -45,7 +47,7 @@ function init() {
         sizes.width = window.innerWidth;
         sizes.height = window.innerHeight;
 
-        camera.aspect = sizes.width / sizes.height;
+        camera.aspect = aspectRatio;
         camera.updateProjectionMatrix();
 
         renderer.setSize(sizes.width, sizes.height);
@@ -61,6 +63,7 @@ function init() {
     {
         pointer.x = (event.clientX / sizes.width) * 2 - 1;
         pointer.y = - (event.clientY / sizes.height) * 2 + 1;
+        meshes[0][0].position.set(0 - pointer.x * 1.5, 0 - pointer.y * 2, -2 + pointer.x * 1.5);
     });
 
     // RENERER
@@ -69,25 +72,26 @@ function init() {
         alpha: true
     });
     renderer.setSize(sizes.width, sizes.height);
+    renderer.setClearColor(0xcccccc, 1);
     document.body.appendChild(renderer.domElement);
 
     // CAMERA
-    const aspectRatio = sizes.width / sizes.height;
+    aspectRatio = sizes.width / sizes.height;
     camera = new THREE.PerspectiveCamera( 
         45, aspectRatio, 0.1, 1000 );
-    camera.position.set(0, 0, 100);
+    camera.position.set(15, 15, 15);
     camera.lookAt(0, 0, 0);
     scene.add(camera);
 
-    controls = new OrbitControls(camera, renderer.domElement);
+    // controls = new OrbitControls(camera, renderer.domElement);
 
     // PLANE
-    const planeGeometry = new THREE.PlaneGeometry(10000, 10000, 1, 1);
-    const planeMaterial = new THREE.MeshStandardMaterial();
-    planeMaterial.setValues({color: 0x222222});
-    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    plane.position.set(0, 0, -150);
-    scene.add(plane);
+    // const planeGeometry = new THREE.PlaneGeometry(10000, 10000, 1, 1);
+    // const planeMaterial = new THREE.MeshStandardMaterial();
+    // planeMaterial.setValues({color: 0xffffff});
+    // const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    // plane.position.set(0, 0, -150);
+    // scene.add(plane);
     
     // LIGHT
 
@@ -107,15 +111,15 @@ function init() {
     const dLightR = new THREE.DirectionalLight(0xffffff, 0.5);
     dLightR.position.set(10, 5, 0);
     scene.add(dLightR);
-    const dLightL = new THREE.DirectionalLight(0xffffff, 0.5);
+    const dLightL = new THREE.DirectionalLight(0xffffff, 0.3);
     dLightL.position.set(-10, 0, 0);
     scene.add(dLightL);
-    dLightTop = new THREE.DirectionalLight(0xffffff, 0.5);
+    const dLightTop = new THREE.DirectionalLight(0xffffff, 0.5);
     dLightTop.position.set(0, 10, 0);
     scene.add(dLightTop);
     const dLightBot = new THREE.DirectionalLight(0xffffff, 0.6);
-    dLightBot.position.set(0, -5, 10);
-    scene.add(dLightBot);
+    // dLightBot.position.set(0, -5, 10);
+    // scene.add(dLightBot);
     // const dLight5 = new THREE.DirectionalLight(0xffffff, 0.5);
     // dLight5.position.set(0, -10, 0);
     // scene.add(dLight5);
@@ -123,26 +127,36 @@ function init() {
     // dLight6.position.set(0, 0, -10);
     // scene.add(dLight6);
 
-    // aLight = new THREE.AmbientLight(0xffffff, 0);
-    // scene.add(aLight);
+    const hLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.2);
+    scene.add(hLight);
 
     // GEOMETRY
     const detalization = 15;
-    // const geometry1 = new THREE.BoxGeometry( 2, 2, 2, 1, 1, 5);
-    // const geometry2 = new THREE.IcosahedronGeometry(10, detalization);
-
-    // const parametricGeometry = new ParametricGeometry(ParametricGeometries.klein, 10, 10);
 
     // MATERIAL
-    // const material = new THREE.MeshPhongMaterial({ wireframe: false});
-    defaultColor = 0x888888;
-    // material.setValues({color: defaultColor});
+    defaultColor = 0xaaaaaa;
+
+    // SHADER
+    // const shader = new THREE.ShaderMaterial({
+    //     uniforms: {
+
+    //     },
+    //     vertexShader:   document.getElementById('mesh-vertex-shader').textContentm,
+    //     fragmentShader: document.getElementById('mesh-fragment-shader').textContent
+    // });
 
     // MESH
-    // const mesh = new THREE.Mesh(geometry2, material);
-    // mesh.material.setValues({wireframe})
+    radius = 3;
+    vertexNormals = true;
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load("/src/texture.jpg");
+    texture.anisotropy = 16;
+    scale = {
+        x: 1.4,
+        y: 1,
+        z: 1.4
+    }
 
-    // mesh.geometry.
     rows = 1;
     cols = 1;
     meshes = Array(rows);
@@ -150,8 +164,8 @@ function init() {
         meshes[i] = Array(cols);
         for (let j = 0; j < cols; j++)
         {
-            let geometry2 = new THREE.IcosahedronGeometry(10, detalization); 
-            let material = new THREE.MeshPhongMaterial({ wireframe: false});
+            let geometry2 = new THREE.IcosahedronGeometry(radius, detalization); 
+            let material = new THREE.MeshPhongMaterial({ wireframe: false, map: texture });
             material.setValues({color: defaultColor});
             meshes[i][j] = new THREE.Mesh(geometry2, material);
             meshes[i][j].position.set(j*30-30*cols/2-(i%2*15), i*30-30*rows/2, -2);
@@ -159,6 +173,19 @@ function init() {
             meshes[i][j].material.opacity = 1;
             scene.add(meshes[i][j]);
         }
+    }
+    if (rows === 1 && cols === 1) {
+        meshes[0][0].position.set(0, 0, -2);
+        meshes[0][0].scale.x = scale.x;
+        meshes[0][0].scale.y = scale.y;
+        meshes[0][0].scale.z = scale.z;
+
+        gsap.fromTo(meshes[0][0].scale, {x: scale.x, y: scale.y, z: scale.z}, {x: scale.x * 1.3, y: scale.y * 0.8, z: scale.z * 1.3, duration: 5, ease: 'expo.inOut'});
+        let tlScale = gsap.timeline({repeat: -1, repeatDelay: 0});
+        tlScale.startTime(5);
+        tlScale.to(meshes[0][0].scale, {x: scale.x * 0.7, y: scale.y * 1.4, z: scale.z * 0.7, duration: 5, ease: 'expo.inOut'})
+        // .to(meshes[0][0].scale, {x: scale.x * 1.1, y: scale.y * 1.3, z: scale.z * 0.9, duration: 2, ease: 'expo.in'})
+        .to(meshes[0][0].scale, {x: scale.x * 1.3, y: scale.y * 0.8, z: scale.z * 1.3, duration: 5, ease: 'expo.inOut'});
     }
     // meshes[0].material.setValues({opacity: 0});
     // meshes[0].material.transparent = true;
@@ -175,53 +202,51 @@ function init() {
     // scene.add(meshes[3]);
 
     // ANIMATION
+
+    // let tl = gsap.timeline({repeat: -1, repeatDelay: 1});
     
-    let tl = gsap.timeline({repeat: -1, repeatDelay: 1});
+    // for (let i = 0; i < meshes.length; i++)
+    //     for (let j = 0; j < meshes[i].length; j++) {
+    //         tl.fromTo(meshes[i][j].material, {opacity: 1}, {duration: 1, ease: 'sine.in', opacity: 0}, 0)
+    //         .to(meshes[i][j].material, {duration: 1, ease: 'sine.in', opacity: 1}, '>');
 
+    //         // gsap.to(meshes[i][j].material, {wireframe: true, duration: 0, ease: 'steps (1)'}, 0);
+    //         let tlFrame1 = gsap.timeline({repeat: -1, repeatDelay: 3});
+    //         tlFrame1.startTime(1);
+    //         tlFrame1.to(meshes[i][j].material, {wireframe: false, duration: 3, ease: 'steps (1)'})
+    //         .to(meshes[i][j].material, {wireframe: true, duration: 0, ease: 'steps (1)'});
+    //         let tlFrame2 = gsap.timeline({repeat: -1, repeatDelay: 3});
+    //         tlFrame2.startTime(4);
+    //         tlFrame2.fromTo(meshes[i][j].material, {wireframe: true}, {wireframe: false, duration: 0, ease: 'steps (1)'})
+    //         .to(meshes[i][j].material, {wireframe: true, duration: 3, ease: 'steps (1)'});
     
-    for (let i = 0; i < meshes.length; i++)
-        for (let j = 0; j < meshes[i].length; j++) {
-            tl.fromTo(meshes[i][j].material, {opacity: 1}, {duration: 1, ease: 'sine.in', opacity: 0}, 0)
-            .to(meshes[i][j].material, {duration: 1, ease: 'sine.in', opacity: 1}, '>');
+    //     }
 
-            // gsap.to(meshes[i][j].material, {wireframe: true, duration: 0, ease: 'steps (1)'}, 0);
-            let tlFrame1 = gsap.timeline({repeat: -1, repeatDelay: 3});
-            tlFrame1.startTime(1);
-            tlFrame1.to(meshes[i][j].material, {wireframe: false, duration: 3, ease: 'steps (1)'})
-            .to(meshes[i][j].material, {wireframe: true, duration: 0, ease: 'steps (1)'});
-            let tlFrame2 = gsap.timeline({repeat: -1, repeatDelay: 3});
-            tlFrame2.startTime(4);
-            tlFrame2.fromTo(meshes[i][j].material, {wireframe: true}, {wireframe: false, duration: 0, ease: 'steps (1)'})
-            .to(meshes[i][j].material, {wireframe: true, duration: 3, ease: 'steps (1)'});
-    
-        }
-
-    let tl2 = gsap.timeline({repeat: -1});
-    tl2.fromTo(dLightTop.color, {r: 1, g: 0.5, b: 0}, {r: 0.5, g: 1, b: 0.2, duration: 1, ease: 'linear'})
-    .to(dLightTop.color, {r: 0.2, g: 0.5, b: 1, duration: 1, ease: 'linear'})
-    .to(dLightTop.color, {r: 1, g: 0.5, b: 0, duration: 1, ease: 'linear'});
-    let tl3 = gsap.timeline({repeat: -1});
-    tl3.fromTo(dLightL.color, {r: 0.2, g: 0.5, b: 1}, {r: 1, g: 0.5, b: 0, duration: 1, ease: 'linear'})
-    .to(dLightL.color, {r: 0.5, g: 1, b: 0.2, duration: 1, ease: 'linear'})
-    .to(dLightL.color, {r: 0.2, g: 0.5, b: 1, duration: 1, ease: 'linear'});
-    let tl4 = gsap.timeline({repeat: -1});
-    tl4.fromTo(dLightBot.color, {r: 0.5, g: 1, b: 0.2}, {r: 0.2, g: 0.5, b: 1, duration: 1, ease: 'linear'})
-    .to(dLightBot.color, {r: 1, g: 0.5, b: 0, duration: 1, ease: 'linear'})
-    .to(dLightBot.color, {r: 0.5, g: 1, b: 0.2, duration: 1, ease: 'linear'});
-    let tl5 = gsap.timeline({repeat: -1});
-    tl5.fromTo(dLightR.color, {r: 0.6, g: 0.7, b: 0.3}, {r: 0.3, g: 0.9, b: 0.1, duration: 1})
-    .to(dLightR.color, {r: 0, g: 0.4, b: 0.8, duration: 1, ease: 'linear'})
-    .to(dLightR.color, {r: 0.6, g: 0.7, b: 0.3, duration: 1, ease: 'linear'});
-
+    // let tl2 = gsap.timeline({repeat: -1});
+    // tl2.fromTo(dLightTop.color, {r: 1, g: 0.5, b: 0}, {r: 0.5, g: 1, b: 0.2, duration: 1, ease: 'linear'})
+    // .to(dLightTop.color, {r: 0.2, g: 0.5, b: 1, duration: 1, ease: 'linear'})
+    // .to(dLightTop.color, {r: 1, g: 0.5, b: 0, duration: 1, ease: 'linear'});
+    // let tl3 = gsap.timeline({repeat: -1});
+    // tl3.fromTo(dLightL.color, {r: 0.2, g: 0.5, b: 1}, {r: 1, g: 0.5, b: 0, duration: 1, ease: 'linear'})
+    // .to(dLightL.color, {r: 0.5, g: 1, b: 0.2, duration: 1, ease: 'linear'})
+    // .to(dLightL.color, {r: 0.2, g: 0.5, b: 1, duration: 1, ease: 'linear'});
+    // let tl4 = gsap.timeline({repeat: -1});
+    // tl4.fromTo(dLightBot.color, {r: 0.5, g: 1, b: 0.2}, {r: 0.2, g: 0.5, b: 1, duration: 1, ease: 'linear'})
+    // .to(dLightBot.color, {r: 1, g: 0.5, b: 0, duration: 1, ease: 'linear'})
+    // .to(dLightBot.color, {r: 0.5, g: 1, b: 0.2, duration: 1, ease: 'linear'});
+    // let tl5 = gsap.timeline({repeat: -1});
+    // tl5.fromTo(dLightR.color, {r: 0.6, g: 0.7, b: 0.3}, {r: 0.3, g: 0.9, b: 0.1, duration: 1})
+    // .to(dLightR.color, {r: 0, g: 0.4, b: 0.8, duration: 1, ease: 'linear'})
+    // .to(dLightR.color, {r: 0.6, g: 0.7, b: 0.3, duration: 1, ease: 'linear'});
 
     
     // REFRESH & ANIMATE
     clock = new THREE.Clock();
 
     velocities = {
-        x: Array.from(Array(1), () => randFloat(-1, 1)),
-        y: Array.from(Array(1), () => randFloat(-1, 1)),
-        z: Array.from(Array(1), () => randFloat(-1, 1))
+        x: Array.from(Array(1), () => randFloat(-0.5, 0.5)),
+        y: Array.from(Array(1), () => randFloat(-0.5, 0.5)),
+        z: Array.from(Array(1), () => randFloat(-0.5, 0.5))
     }
 
     intersectedObjects = Array.from(meshes.flat(), mesh => false);
@@ -237,12 +262,13 @@ function tick() {
 
             // mesh.material.opacity = 0.7;
     // });
+    // console.log(currentScale);
 
     for (let i = 0; i < meshes.length; i++)
         for (let j = 0; j < meshes[i].length; j++) {
-            meshes[i][j].rotation.x += velocities.x[0] * delta;
+            meshes[i][j].rotation.x += delta * 0.3;
             meshes[i][j].rotation.y += velocities.y[0] * delta;
-            meshes[i][j].rotation.z += velocities.z[0] * delta;
+            // meshes[i][j].rotation.z += delta * -0.3;
         
             const position = meshes[i][j].geometry.attributes.position;
             const v = new THREE.Vector3();
@@ -252,9 +278,9 @@ function tick() {
                 let time = Date.now();
                 v.normalize();
                 let distance = meshes[0][0].geometry.parameters.radius + noise(
-                    v.x + time * 0.0003,
-                    v.y + time * 0.0003,
-                    v.z + time * 0.0003) * blobScale;
+                    v.x + time * 0.0005,
+                    v.y + time * 0.0005,
+                    v.z + time * 0.0005) * blobScale;
                 v.multiplyScalar(distance);
                 position.setXYZ(k, v.x, v.y, v.z);
             }
@@ -262,7 +288,8 @@ function tick() {
             meshes[i][j].geometry.verticesNeedUpdate = true;
             meshes[i][j].geometry.attributes.position.needsUpdate = true;
             meshes[i][j].geometry.normalsNeedUpdate = true;
-            // meshes[i][j].geometry.computeVertexNormals();
+            if (vertexNormals)
+                meshes[i][j].geometry.computeVertexNormals();
             // meshes[i][j].geometry.computeBoundingSphere();
             // meshes[0].geometry.computeVertexNormals();
         }
@@ -272,9 +299,16 @@ function tick() {
 
     for (let i = 0; i < meshes.length; i++)
         for (let j = 0; j < meshes[i].length; j++) {
-            if (!intersects.includes(meshes[i][j])) {
+            if (!(intersects.map((obj) => obj.object === meshes[i][j])).includes(true)) {
                 if (intersectedObjects[i*rows+j]) {
+                    console.log(intersects);
+                    console.log(meshes[i][j]);
+                    console.log(intersects.includes(meshes[i][j]));
                     // meshes[0].geometry.scale(1/1.2, 1/1.2, 1/1.2);
+                    // const scale_ = meshes.flat()[i*rows+j].scale;
+                    const params = meshes.flat()[i*rows+j].geometry.parameters;
+                    // gsap.to(scale_, {duration: 0.3, x: scale_.x/1.2, y: scale_.y/1.2, z: scale_.z/1.2});
+                    gsap.to(params, {duration: 0.3, radius: radius});
                     intersectedObjects[i*rows+j] = false;
                 }
                 meshes[i][j].material.color.set(defaultColor);
@@ -290,17 +324,24 @@ function tick() {
         // if (!intersectedObjects[i*rows+j]) {
         let index = meshes.flat().findIndex((obj) => {return obj === intersects[k].object});
         if (!intersectedObjects[index]) {
+            // console.log(intersectedObjects[index]);
         // console.log(intersects[k]);
         // console.log(meshes.flat());
         // console.log(meshes.flat().find((obj) => {return obj === intersects[k].object}))
-            intersects[k].object.geometry.scale(1.2, 1.2, 1.2);
+            // intersects[k].object.geometry.scale(1.2, 1.2, 1.2);
+            // const scale_ = intersects[k].object.scale;
+            // gsap.to(scale_, {duration: 0.3, x: scale_.x*1.2, y: scale_.y*1.2, z: scale_.z*1.2});
+            const params = intersects[k].object.geometry.parameters;
+            gsap.to(params, {duration: 0.3, radius: radius * 1.2});
+            // console.log(intersects[k].object.scale);
+            // intersects[k].object.scale.x = 2;
             intersectedObjects[index] = true;
             // console.log(intersects[i].object);
             // console.log(intersectedObjects[i]);
         }
     }
 
-    controls.update();
+    // controls.update();
     renderer.render(scene, camera);
     window.requestAnimationFrame(tick);
     // console.log(sizes.width);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const path = require('path');
 
 module.exports = {
@@ -8,21 +7,15 @@ module.exports = {
         path: path.resolve(__dirname, 'src'),
         // library: 'three',
     },
-    watch: true,
-    mode: 'development'
-    // mode: 'production'
-=======
-const path = require('path');
-
-module.exports = {
-    entry: './src/js/three.js',
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'src'),
-        // library: 'three',
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resourse',
+            },
+        ],
     },
     watch: true,
     mode: 'development'
     // mode: 'production'
->>>>>>> origin/main
 }
